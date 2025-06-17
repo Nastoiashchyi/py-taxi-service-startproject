d0ef2cec1a26555776eefd82f06e9cc826825818
Yevhen Nastoiashchyi <Nastoyashiy@i.ua>
2025-06-17 23:16:08 +0300

    solution

--- taxi/admin.py
+++ taxi/admin.py
@@ -1,38 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from taxi.models import Manufacturer, Driver, Car
 
 
-@admin.register(Manufacturer)
-class ManufacturerAdmin(admin.ModelAdmin):
-    list_display = ("name",)
-    search_fields = ("name",)
+admin.site.register(Manufacturer)
 
 
 @admin.register(Driver)
-class DriverAdmin(admin.ModelAdmin):
-    list_display = ("username", "license_number")
-    search_fields = ("username", "license_number")
-    fieldsets = (
-        (None, {"fields": ("username", "password")}),
-        ("Personal info", {"fields": ("first_name", "last_name", "email")}),
-        (
-            "Permissions",
-            {"fields": ("is_active", "is_staff", "is_superuser")}
-        ),
-        ("Additional info", {"fields": ("license_number",)}),
-    )
-    add_fieldsets = (
-        (None, {
-            "classes": ("wide",),
-            "fields": ("username", "password1", "password2"),
-        }),
-        ("Additional info", {"fields": ("license_number",)}),
-    )
+class DriverAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("license_number",)
+    fieldsets = UserAdmin.fieldsets + (("Additional info", {"fields": ("license_number",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (("Additional info", {"fields": ("license_number",)}),)
 
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
-    list_display = ("model", "manufacturer")
-    search_fields = ("model",)
-    list_filter = ("manufacturer",)
+    search_fields = ("model", )
+    list_filter = ("manufacturer__name", )
--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -106,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "Europe/Kiev"
-USE_I18N = True
 
 USE_I18N = True
 
